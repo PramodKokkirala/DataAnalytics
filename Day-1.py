b6a3eb1609398178143e2f8978054b8da30dd24b
KOKKIRALA PRAMOD ANJI SRINIVAS <pramodkokkirala823@gmail.com>
2026-05-13 17:35:11 +0530

    Day-1 Python Basics
    #Python #Data Analyst

---
+++ Day-1.py
@@ -0,0 +1,68 @@
+print("This is my First Python Class")
+
+num=23
+so="python"
+any=[1,2]
+an={"Name": "Teja"}
+print(num)
+print(so)
+print(any)
+print(an)
+
+'''
+High-Level-
+No Need to worry about low level Management
+
+
+Dynamically typed language-
+No need to mention the kind of data taking to variable or data types
+
+
+num=23-int
+so="python"-string
+any=[1,2]-list
+an={"Name": "Teja"}-key value pair
+print(num)
+print(so)
+print(any)
+print(an)
+
+
+Interpreter Language-
+Python will execute the code line-by-line and can throw the error if any but all the lines before error will be executed
+
+
+Tokens-(Keywords)
+Keywords in the program
+Example-if,else,for,while,and,or,is,...
+
+
+Identifiers-
+Names of variables, functions and classes
+1)Variables-
+*GoodWays:A-Z,a-z,0-9(not at first position),_
+*BadWays:Special Characters,Space,Numbers
+
+
+Comments-
+1.Single-line:(#)
+2.Multi-line:('''''')
+
+
+Boolean-
+True or False
+'''
+
+
+
+
+Employee_Name="Pramod"
+#Employee Name="Chaitanya"
+Age=40
+#@ge=40
+print(Employee_Name)
+print(Age)
+
+a=3
+b=4
+print(a!=b)
